0cc590de4d46aecfc469dd434140e6cb2b0a50a7
Alifur Rahman <alifurrahman1999@gmail.com>
2024-01-20 01:48:46 +0600

    init files

---
+++ layouts/scripts.php
@@ -0,0 +1,20 @@
+<!-- BOOTSTRAP JS -->
+<script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- POPPER JS -->
+<script src="assets/libs/@popperjs/core/umd/popper.min.js"></script>
+
+<!-- DEFAULTMENU JS -->
+<script src="assets/js/defaultmenu.js"></script>
+
+<!-- CATEGORYMENU JS -->
+<script src="assets/js/category-menu.js"></script>
+
+<!-- ACCEPT-COOKIE JS -->
+<script src="assets/js/cookies.js"></script>
+
+<!-- SIMONWEP-PICKER JS -->
+<script src="assets/libs/@simonwep/pickr/pickr.es5.min.js"></script>
+
+<!-- CUSTOM-SWITCHER JS -->
+<script src="assets/js/custom-switcher.js"></script>
---
+++ layouts/styles.php
@@ -0,0 +1,15 @@
+<!-- BOOTSTRAP CSS -->
+<link id="style" href="assets/libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- STYLE CSS -->
+<link href="assets/css/styles.css" rel="stylesheet">
+
+<!-- SIMONWEP-PICKER CSS -->
+<link href="assets/libs/@simonwep/pickr/themes/classic.min.css" rel="stylesheet">
+<link href="assets/libs/@simonwep/pickr/themes/monolith.min.css" rel="stylesheet">
+<link href="assets/libs/@simonwep/pickr/themes/nano.min.css" rel="stylesheet">
+
+<!-- ICONS CSS -->
+<link href="assets/css/icons.css" rel="stylesheet">
+<!-- CUSTOM CSS -->
+<link href="assets/css/custom.css" rel="stylesheet">
